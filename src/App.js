5adc0b3e8e59ec41c276d54c9fc75d8645654dbb
Mikko Tuominen <mikko.r.tuominen@gmail.com>
2020-05-17 00:09:51 +0300

    refactored App into stateful components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-class RunButton extends React.Component {
-  handleClick() {
-    console.log('this is:', this);
-    alert('todo...');
+const playersDefaultValue = `player1\nplayer2\nplayer3\nplayer4`
+const rankingsDefaultValue = `player1:80\nplayer2:60\nplayer3:40\nplayer4:20`
+
+const App = (props) => {
+  const [data, setData] = useState({
+    fair: true,
+    players: playersDefaultValue,
+    rankings: rankingsDefaultValue,
+    teamRed: [],
+    teamWhite: []
+  })
+
+  const updateData = () => {
+    let players = data.players.split('\n')
+    let teamRed = []
+    let teamWhite = []
+
+    if (data.fair && players.length > 1) {
+      // swap position of top two players before making team rosters to equalize rosters a bit
+      [players[0], players[1]] = [players[1], players[0]]
+    }
+
+    for (let i = 0; i < players.length; i = i + 2) {
+      teamRed.push(players[i])
+    }
+
+    for (let i = 1; i < players.length; i = i + 2) {
+      teamWhite.push(players[i])
+    }
+
+    const newData = {
+      ...data,
+      teamRed: teamRed,
+      teamWhite: teamWhite
+    }
+    setData(newData)
   }
 
-  render() {
-    return (
-      <button className="button" onClick={() => this.handleClick()}>
-        Run
-      </button>
-    );
-  }
-}
-
-function App() {
-  let players = `player1\nplayer2\nplayer3\nplayer4`
-  let rankings = `player1:80\nplayer2:60\nplayer3:40\nplayer4:20`
   return (
     <div className="App">
       <header className="App-header">
         <p>Rosters</p>
       </header>
       <div className="App-content">
-        <div id="reds">
-          <div className="team-name">Team #1</div>
-          <div className="players-list">player1, player4</div>
-        </div>
-        <div id="whites">
-          <div className="team-name">Team #2</div>
-          <div className="players-list">player2, player3</div>
-        </div>
+        <Team id="reds" name="Red" players={data.teamRed} />
+        <Team id="whites" name="White" players={data.teamWhite} />
         <div className="row">
           <div id="players-column" className="column">
-            <div>Players</div>
-            <textarea id="players" className="resize-none" rows="20" cols="12">{players}</textarea>
+            <InputTextArea
+              id="players"
+              label="Players"
+              defaultValue={data.players} />
           </div>
           <div id="rankings-column" className="column">
-            <div>Rankings</div>
-            <textarea id="rankings" className="resize-none" rows="20" cols="16">{rankings}</textarea>
+            <InputTextArea
+              id="rankings"
+              label="Rankings"
+              defaultValue={data.rankings} />
           </div>
-          <div className="column flex-end">
-            <RunButton />
+          <div id="action-column" className="column flex-end">
+            <Button handleClick={updateData} label='Run' />
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+const Button = (props) => {
+  return (
+    <button className="button" onClick={props.handleClick}>
+      {props.label}
+    </button>
+  )
+}
+
+const InputTextArea = (props) => {
+  return (
+    <div id={props.id}>
+      <div>{props.label}</div>
+      <textarea
+        className="resize-none"
+        rows="20"
+        cols="16"
+        defaultValue={props.defaultValue} />
+    </div>
+  )
+}
+
+const Team = (props) => {
+  return (
+    <div id="{props.id}">
+      <div className="team-name">Team {props.name}</div>
+      <div className="players-list">{props.players.join(', ')}</div>
+    </div>
+  )
 }
 
 export default App;
